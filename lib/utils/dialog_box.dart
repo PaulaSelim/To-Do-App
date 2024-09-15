b3cb39f84b1fe589eab02ac6f7b7f20b51d8ec23
PaulaSelim <s.paulakhalil@icloud.com>
2024-09-15 20:46:38 +0300

    finished DialogBox widget UI

--- lib/utils/dialog_box.dart
+++ lib/utils/dialog_box.dart
@@ -1,31 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:to_do/utils/my_button.dart';
 
 class DialogBox extends StatelessWidget {
-  const DialogBox({super.key});
+  final controller;
+  VoidCallback onSave;
+  VoidCallback onCancel;
+
+  DialogBox({
+    super.key,
+    required this.controller,
+    required this.onSave,
+    required this.onCancel,
+  });
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: const Text('Add a new task'),
-      content: TextField(
-        decoration: InputDecoration(
-          hintText: 'Enter a task',
+      content: Container(
+        height: 120.0,
+        width: 400.0,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // get user input
+
+            TextField(
+              controller: controller,
+              decoration: const InputDecoration(
+                hintText: 'Task name',
+              ),
+            ),
+
+            // buttons
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                //add button
+
+                MyButton(text: "Cancel", onPressed: onCancel),
+
+                //space between buttons
+
+                const SizedBox(width: 10.0),
+
+                //cancel button
+
+                MyButton(text: "Save", onPressed: onSave),
+              ],
+            ),
+          ],
         ),
       ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Cancel'),
-        ),
-        TextButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Add'),
-        ),
-      ],
     );
   }
 }
